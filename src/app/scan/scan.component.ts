5ab90df8f94ed8c00e2f391d1a813768120172f8
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-10-28 08:44:36 +0630

    a

--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { BarcodeFormat } from "@zxing/library";
 
 @Component({
   selector: "app-scan",
@@ -7,7 +8,56 @@ import { Component, OnInit } from "@angular/core";
 export class ScanComponent implements OnInit {
   allowed = false;
 
+  availableDevices: MediaDeviceInfo[] = [];
+  deviceCurrent?: MediaDeviceInfo;
+  deviceSelected = "";
+
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+
+  hasDevices = false;
+  hasPermission = false;
+  tryHarder = false;
+
+  qrResultString = "";
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const selectedStr = selected || "";
+    if (this.deviceSelected === selectedStr) {
+      return;
+    }
+    this.deviceSelected = selectedStr;
+    const device = this.availableDevices.find((x) => x.deviceId === selected);
+    this.deviceCurrent = device || undefined;
+  }
+
+  onDeviceChange(device: MediaDeviceInfo) {
+    const selectedStr = device?.deviceId || "";
+    if (this.deviceSelected === selectedStr) {
+      return;
+    }
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
 }
