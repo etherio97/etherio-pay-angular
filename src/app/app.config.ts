1cf64367236a337e71e9b475f716c3e0118db2e5
Etherio <50423290+etherio97@users.noreply.github.com>
2022-02-17 13:00:25 +0630

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,11 @@ import { FirebaseOptions } from '@firebase/app';
 let HOSTNAME = window.location.hostname;
 let BASE_URL = '/api';
 
-if (!HOSTNAME.includes('.vercel.app')) {
+if (!HOSTNAME.includes('etherio.fun')) {
   BASE_URL = 'https://etherio-pay.herokuapp.com';
 
   if (!HOSTNAME.match(/^(localhost|\d)/)) {
-    window.location.href = 'https://etherio-pay.vercel.app';
+    window.location.href = 'https://pay.etherio.fun';
   }
 }
 
