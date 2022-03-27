38dd84095acf4cda3c946b829b3aeb9479273fbb
etherio97 <ethereal97@gmail.com>
2022-03-27 17:55:04 +0630

    bug fix error

--- src/app/pages/transfer/transfer.service.ts
+++ src/app/pages/transfer/transfer.service.ts
@@ -2,23 +2,23 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { SERVICE_URL } from 'src/app/app.config';
-import { SendTransferDto } from './transfer.model.ts'
+import { SendTransferDto } from './transfer.model';
 
 @Injectable({
   providedIn: "root",
 })
 export class TransferService {
-  private _findAccountsUrl: string = SERVICE_URL;
+  private _findAccountsUrl: string = SERVICE_URL.FIND_ACCOUNTS;
   
   private _sendTransferUrl: string = SERVICE_URL.SEND_TRANSFER;
   
-  constructor(private http: HttpClient) {}
+  constructor(private _httpClient: HttpClient) {}
 
   findAccounts(identifier: string): Observable<string[]> {
-    return this.http.post(this._findAccountsUrl, { identifier });
+    return this._httpClient.post(this._findAccountsUrl, { identifier });
   }
   
-  sendTransfer(sendTransferDto: sendTransferDto) {
+  sendTransfer(sendTransferDto: SendTransferDto) {
     return this.http.post(this._sendTransferUrl, sendTransferDto);
   }
 }
